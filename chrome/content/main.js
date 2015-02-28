244964bcc085eefee8f74497d8153d175bdd8556
Turn Off the Lights <stefanvandamme@stefanvd.net>
2015-02-28 20:56:09 +0100

    v3.1.0.3

--- chrome/content/main.js
+++ chrome/content/main.js
@@ -40,7 +40,7 @@ if ("undefined" == typeof(TurnOfftheLights)) {
     ns.init = function () {
 // welcome page
 var firstrun = Services.prefs.getBoolPref("extensions.TurnOfftheLights.firstrun");
-var curVersion = "3.1.0.1";
+var curVersion = "3.1.0.3";
 
 if (firstrun) {
 // Adding button by default ------------
